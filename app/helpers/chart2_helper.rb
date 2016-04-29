0d8f748874cad08e42c94800607abe6854f691f9
ytbryan <ytbryan@gmail.com>
2016-04-30 00:48:26 +0800

    added a few method skeleton for chart2.js

--- app/helpers/chart2_helper.rb
+++ app/helpers/chart2_helper.rb
@@ -1,3 +1,89 @@
 module Chart2Helper
 
+  def chart2_bar
+    html = "".html_safe
+    script = javascript_tag do
+      <<-END.html_safe
+      END
+    end
+    return html + script
+  end
+
+
+  def chart2_line
+    html = "".html_safe
+    script = javascript_tag do
+      <<-END.html_safe
+      END
+    end
+    return html + script
+  end
+
+  def chart2_scatter
+    html = "".html_safe
+    script = javascript_tag do
+      <<-END.html_safe
+      END
+    end
+    return html + script
+  end
+
+  def chart2_radar
+    html = "".html_safe
+    script = javascript_tag do
+      <<-END.html_safe
+      END
+    end
+    return html + script
+  end
+
+  def chart2_pie
+    html = "".html_safe
+    script = javascript_tag do
+      <<-END.html_safe
+      END
+    end
+    return html + script
+  end
+
+  def chart2_donut
+    html = "".html_safe
+    script = javascript_tag do
+      <<-END.html_safe
+      END
+    end
+    return html + script
+  end
+
+  def chart2_scale
+    html = "".html_safe
+    script = javascript_tag do
+      <<-END.html_safe
+      END
+    end
+    return html + script
+  end
+
+  def chart2_polar_area
+    html = "".html_safe
+    script = javascript_tag do
+      <<-END.html_safe
+      END
+    end
+    return html + script
+  end
+
+
+
+
+
+  # def chart2_scatter
+  #   html = "".html_safe
+  #   script = javascript_tag do
+  #     <<-END.html_safe
+  #     END
+  #   end
+  #   return html + script
+  # end
+
 end
